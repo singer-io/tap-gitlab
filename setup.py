da2de07a04e95025ae9cf5d884ffdb33d47f7729
Chris Capurso <chrisc@stitchdata.com>
2018-01-26 12:04:48 -0500

    bump singer-python to 5.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-gitlab',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_gitlab'],
       install_requires=[
-          'singer-python==1.2.0',
+          'singer-python==5.0.4',
           'requests==2.12.4',
           'strict-rfc3339==0.7',
           'backoff==1.3.2'
