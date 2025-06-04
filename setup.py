5f386f884ed4e63a0d94e27a4a78f61e547da12e
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-06-04 20:14:29 +0530

    SAC-27682: Fix libraries and dependabot issues in setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,27 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from setuptools import setup
-
-setup(name='tap-gitlab',
-      version='0.5.1',
-      description='Singer.io tap for extracting data from the GitLab API',
-      author='Stitch',
-      url='https://singer.io',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap_gitlab'],
-      install_requires=[
-          'singer-python==5.0.4',
-          'requests==2.20.0',
-          'strict-rfc3339==0.7',
-          'backoff==1.3.2'
-      ],
-      entry_points='''
-          [console_scripts]
-          tap-gitlab=tap_gitlab:main
-      ''',
-      packages=['tap_gitlab'],
-      package_data = {
-          'tap_gitlab/schemas': [
-            "branches.json",
-            "commits.json",
-            "issues.json",
-            "milestones.json",
-            "projects.json",
-            "users.json",
-          ],
-      },
-      include_package_data=True,
+setup(
+    name='tap-gitlab',
+    version='1.0.0',
+    description='Singer.io tap for extracting data from the GitLab API',
+    author='Your Name',
+    url='http://singer.io',
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=find_packages(),  # Will automatically include tap_sample
+    install_requires=[
+        'singer-python==6.1.1',
+        'requests==2.32.3',
+        'strict-rfc3339==0.7',
+        'backoff==2.2.1'
+    ],
+    entry_points='''
+        [console_scripts]
+        tap-gitlab=tap_sample.__init__:main
+    ''',
+    package_data={
+        'tap_sample': ['schemas/*.json'],
+    },
+    include_package_data=True,
 )
