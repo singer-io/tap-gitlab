ca6cbd89a53fd8b730e7dd53bd5193b73a8ee991
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-06-09 13:08:57 +0530

    Add: Unit and integration tests under tests/unittests/

---
+++ tests/base.py
@@ -0,0 +1,114 @@
+import copy
+import os
+import unittest
+from datetime import datetime as dt
+from datetime import timedelta
+
+import dateutil.parser
+import pytz
+from tap_tester import connections, menagerie, runner
+from tap_tester.logger import LOGGER
+from tap_tester.base_suite_tests.base_case import BaseCase
+
+
+class BaseTest(BaseCase):
+    """Setup expectations for test sub classes.
+
+    Metadata describing streams. A bunch of shared methods that are used
+    in tap-tester tests. Shared tap-specific methods (as needed).
+    """    
+    start_date = "2019-01-01T00:00:00Z"
+
+    @staticmethod
+    def tap_name():
+        """The name of the tap."""
+        return "tap-"
+    
+    @staticmethod
+    def get_type():
+        """The name of the tap."""
+        return "platform."
+
+    @classmethod
+    def expected_metadata(cls):
+        """The expected streams and metadata about the streams."""
+        return {
+            "projects": {
+                cls.PRIMARY_KEYS: { "id" },
+                cls.REPLICATION_METHOD: cls.full_table,
+                cls.REPLICATION_KEYS: set(),
+                cls.OBEYS_START_DATE: False,
+                cls.API_LIMIT: 100
+            },
+            "branches": {
+                cls.PRIMARY_KEYS: { "project_id, name" },
+                cls.REPLICATION_METHOD: cls.full_table,
+                cls.REPLICATION_KEYS: set(),
+                cls.OBEYS_START_DATE: False,
+                cls.API_LIMIT: 100
+            },
+            "commits": {
+                cls.PRIMARY_KEYS: { "id" },
+                cls.REPLICATION_METHOD: cls.full_table,
+                cls.REPLICATION_KEYS: set(),
+                cls.OBEYS_START_DATE: False,
+                cls.API_LIMIT: 100
+            },
+            "issues": {
+                cls.PRIMARY_KEYS: { "id" },
+                cls.REPLICATION_METHOD: cls.full_table,
+                cls.REPLICATION_KEYS: set(),
+                cls.OBEYS_START_DATE: False,
+                cls.API_LIMIT: 100
+            },
+            "project_milestones": {
+                cls.PRIMARY_KEYS: { "id" },
+                cls.REPLICATION_METHOD: cls.full_table,
+                cls.REPLICATION_KEYS: set(),
+                cls.OBEYS_START_DATE: False,
+                cls.API_LIMIT: 100
+            },
+            "group_milestones": {
+                cls.PRIMARY_KEYS: { "id" },
+                cls.REPLICATION_METHOD: cls.full_table,
+                cls.REPLICATION_KEYS: set(),
+                cls.OBEYS_START_DATE: False,
+                cls.API_LIMIT: 100
+            },
+            "users": {
+                cls.PRIMARY_KEYS: { "id" },
+                cls.REPLICATION_METHOD: cls.full_table,
+                cls.REPLICATION_KEYS: set(),
+                cls.OBEYS_START_DATE: False,
+                cls.API_LIMIT: 100
+            },
+            "groups": {
+                cls.PRIMARY_KEYS: { "id" },
+                cls.REPLICATION_METHOD: cls.full_table,
+                cls.REPLICATION_KEYS: set(),
+                cls.OBEYS_START_DATE: False,
+                cls.API_LIMIT: 100
+            }
+        }
+
+    @staticmethod
+    def get_credentials():
+        """Authentication information for the test account."""
+        credentials_dict = {}
+        creds = 
+
+        for cred in creds:
+            credentials_dict[cred] = os.getenv(creds[cred])
+
+        return credentials_dict
+
+    def get_properties(self, original: bool = True):
+        """Configuration of properties required for the tap."""
+        return_value = {
+            "start_date": "2022-07-01T00:00:00Z"
+        }
+        if original:
+            return return_value
+
+        return_value["start_date"] = self.start_date
+        return return_value
