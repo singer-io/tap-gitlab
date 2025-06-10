63bac0890d9412af0188cd78dbc264f1385ecd39
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-06-10 13:38:29 +0530

    Fix: add pytz to install requires for singer dependency

--- setup.py
+++ setup.py
@@ -2,33 +2,27 @@
 
 from setuptools import setup
 
-setup(name='tap-gitlab',
-      version='0.5.1',
-      description='Singer.io tap for extracting data from the GitLab API',
-      author='Stitch',
-      url='https://singer.io',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap_gitlab'],
-      install_requires=[
-          'singer-python==5.0.4',
-          'requests==2.20.0',
-          'strict-rfc3339==0.7',
-          'backoff==1.3.2'
-      ],
-      entry_points='''
-          [console_scripts]
-          tap-gitlab=tap_gitlab:main
-      ''',
-      packages=['tap_gitlab'],
-      package_data = {
-          'tap_gitlab/schemas': [
-            "branches.json",
-            "commits.json",
-            "issues.json",
-            "milestones.json",
-            "projects.json",
-            "users.json",
-          ],
-      },
-      include_package_data=True,
+setup(
+    name='tap-gitlab',
+    version='0.5.1',
+    description='Singer.io tap for extracting data from the GitLab API',
+    author='Stitch',
+    url='https://singer.io',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    install_requires=[
+        'singer-python==5.0.4',
+        'requests==2.20.0',
+        'strict-rfc3339==0.7',
+        'backoff==1.3.2',
+        'pytz'
+    ],
+    entry_points='''
+        [console_scripts]
+        tap-gitlab=tap_gitlab:main
+    ''',
+    packages=['tap_gitlab'],
+    package_data={
+        'tap_gitlab': ['schemas/*.json', 'schemas/shared/*.json']
+    },
+    include_package_data=True,
 )
