c52da0b56bac1e81e87d22b9d5644acf40adeebd
Romilqlik <romil.totade@qlik.com>
2025-06-11 10:28:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
     url='https://singer.io',
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     install_requires=[
-        'singer-python==5.0.4',
-        'requests==2.20.0',
+        'singer-python==6.1.1',
+        'requests==2.32.3',
         'strict-rfc3339==0.7',
-        'backoff==1.3.2',
+        'backoff==2.2.1',
         'pytz'
     ],
     entry_points='''
