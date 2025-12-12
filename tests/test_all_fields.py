ba5967cfce90ffcb32f4402154f86f450d4318ad
attiwari <atul.tiwari@qlik.com>
2025-12-12 11:25:17 +0000

    SAC-29767:Updated for integration tests fix

--- tests/test_all_fields.py
+++ tests/test_all_fields.py
@@ -1,14 +1,35 @@
 from base import BaseTest
 from tap_tester.base_suite_tests.all_fields_test import AllFieldsTest
 
-KNOWN_MISSING_FIELDS = {
-
-}
-
 
 class AllFields(AllFieldsTest, BaseTest):
     """Ensure running the tap with all streams and fields selected results in
     the replication of all fields."""
+
+    MISSING_FIELDS = {
+        "groups": {
+            "repository_storage"
+        },
+        "issues": {
+            'assignee_id',
+            'milestone_id',
+            'subscribed',
+            'author_id'
+        },
+        "projects": {
+            'runners_token',
+            'license_url',
+            'mirror_user_id',
+            'only_mirror_protected_branches',
+            'license',
+            'statistics',
+            'mirror_overwrites_diverged_branches',
+            'printing_merge_requests_link_enabled',
+            'owner',
+            'repository_storage',
+            'mirror_trigger_builds',
+        }
+    }
 
     @staticmethod
     def name():
